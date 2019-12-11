54639615cebb8239bdad87761515b79927c69e36
jeanfrancois360 <mjeanfrancois360@gmail.com>
2019-12-11 09:47:29 +0200

    Second commit

---
+++ application/controllers/Inputs_market.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Inputs_market extends CI_Controller{
+    public function __construct()
+    {
+      parent::__construct();
+      $this->load->helper('url');
+    }
+    public function inputs(){
+        
+    }
+
+}
+?>
